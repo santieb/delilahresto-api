d803437f4168fef098de957f13cac5b838a6bae6
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-08-13 00:28:37 -0300

    update

--- middlewares/users.js
+++ middlewares/users.js
@@ -2,7 +2,7 @@ const users = require('../models/users')
 
 const confirmRegistration = (req, res, next) => {
     check = users.filter(user => user.username === req.body.username && user.mail === req.body.mail)
-    if(check.length != 0) res.json({msj: "The username or email is in use"})
+    if(check.length != 0) {res.json({msj: "The username or email is in use"})}
 
     if(req.body.username === ""){ //no funciona esto
         res.json({msj: "Fill in all fields"})
