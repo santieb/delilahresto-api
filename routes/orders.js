36436cf77e5e6d2fed2f2b3bfec8da3a14127d5e
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-08-19 17:06:27 -0300

    update orders

--- routes/orders.js
+++ routes/orders.js
@@ -5,10 +5,7 @@ const users = require('../models/users')
 const orders = require('../models/orders')
 const products = require('../models/products')
 
-//hacer un endpoint para ver los productos
-
-
-router.get('/orders/history/:id',middlewares.confirmId, (req, res) => {
+router.get('/orders/history/:id', middlewares.confirmId, (req, res) => {
     const user = (users.find(users => users.id == req.params.id))
     const order = (orders.ordersList.filter(orders => user.username == orders.username))
 
@@ -17,23 +14,21 @@ router.get('/orders/history/:id',middlewares.confirmId, (req, res) => {
 })
 
 let count = 0;
-router.post('/orders/:id', middlewares.confirmId, middlewares.confirmOrder, (req, res) => {
-    count++
-    number =`#${count}`
-    date = new Date()
-    time = `${date.getHours()}:${date.getMinutes()}`
+router.post('/orders/:id', middlewares.confirmId, middlewares.confirmOrder, (req, res) => { //filter para ver si tiene un pedido pendiente anteriormente o no
     const user = (users.find(users => users.id == req.params.id))
-
+    count++
+    date = new Date()
+    
     req.body.state = orders.states[1]
-    req.body.time = time
-    req.body.number = number
+    req.body.time = `${date.getHours()}:${date.getMinutes()}`
+    req.body.number = `#${count}`
     req.body.username = user.username
     orders.ordersList.push(req.body)
     res.json({msj:`order created`})
 })
 
-router.post('/orders/confirmation/:id ',middlewares.confirmId, (req, res) => { // arreglar
-
+router.post('/orders/confirmation/:id ',middlewares.confirmId, (req, res) => {
+        
     if (req.body.confirm) 
     res.json({msj:`order created`})
 })
