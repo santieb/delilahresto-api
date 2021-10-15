6f3e7565238190afb2c4995101ab1d88b4657291
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-10-15 16:17:26 -0300

    [add] orderStates.model

--- src/models/orderStates.models.js
+++ src/models/orderStates.models.js
@@ -2,14 +2,10 @@ const mongoose = require("../config/db.config");
 const { Schema } = mongoose;
 
 const orderStatusScheme = new Schema({
-    state: { type: String, required: true },
+    state: { type: String, required: true }
 });
 
 const orderStatus = mongoose.model("orderStatus", orderStatusScheme);
-
-orderStatus.create( {
-    state: "hola"
-})
 
 
 module.exports = orderStatus;
